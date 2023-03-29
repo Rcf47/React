ce4081db538d523b558bcb52051cbd68daebcf1c
Vadim Cherepanov <xenos26@gmail.com>
2023-03-30 04:53:55 +0700

    feat: add main text

--- scrimba/Project24WithVite/components/Main.jsx
+++ scrimba/Project24WithVite/components/Main.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 function Main() {
   return (
-    <h1>
-      Main goes here
-    </h1>
+    <main>
+      <h1>
+        Fun facts about React
+      </h1>
+      <ul className="list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Power thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
   )
 }
 
