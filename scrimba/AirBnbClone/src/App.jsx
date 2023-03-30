5945ef7938e60499084212c025b23942fa6da861
Vadim Cherepanov <xenos26@gmail.com>
2023-03-31 06:14:29 +0700

    fix: fix App

--- scrimba/AirBnbClone/src/App.jsx
+++ scrimba/AirBnbClone/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Navbar from '../components/Navbar'
+import Main from '../components/Main'
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
       <Navbar />
+      <Main />
     </>
   )
 }
