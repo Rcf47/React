29017e3458fa7e9125bc16af8287ceea543896ef
Vadim Cherepanov <xenos26@gmail.com>
2023-03-31 08:08:32 +0700

    feat: add style for main

--- scrimba/AirBnbClone/components/Main.jsx
+++ scrimba/AirBnbClone/components/Main.jsx
@@ -2,12 +2,12 @@ import './Main.css'
 
 function Main() {
   return (
-    <main className='main__body'>
+    <section className='main__body'>
       <img src='../public/airbnb-photo.png' className='main__photo' />
       <h2 className='main__article--title'>Online Experiences</h2>
       <p className='main__article--text'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
       </p>
-    </main>
+    </section>
   )
 }
 
