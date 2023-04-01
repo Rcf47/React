10961d74a083ff46d64376c2f67efd8905a32d9a
Vadim Cherepanov <xenos26@gmail.com>
2023-04-01 08:16:19 +0700

    feat: add styles for Card

--- scrimba/AirBnbClone/components/Card.jsx
+++ scrimba/AirBnbClone/components/Card.jsx
@@ -3,12 +3,13 @@ import "./Card.css";
 function Card() {
   return (
     <section className="card__body">
+      <label className="card__label" >SOLD OUT</label>
       <img src="../public/katie-zaferes.png" className="card__photo" />
       <article className="card__description">
         <div className="card__rating">
           <img src="../public/Star.png" className="card__star" />
           <span>5.0</span>
-          <span>(6)</span>
+          <span>(6) • </span>
           <span>USA</span>
         </div>
         <p className="card__text">Life lessons with Katie Zaferes</p>
@@ -17,5 +18,12 @@ function Card() {
     </section>
   );
 }
+const person = {
+  name: 'Vadim',
+  lastname: 'Cherepanov',
+  age: 34,
+  diet: 'I don\'t have many'
+}
+
 
 export default Card;
--- scrimba/AirBnbClone/components/Main.jsx
+++ scrimba/AirBnbClone/components/Main.jsx
@@ -4,7 +4,7 @@ function Main() {
   return (
     <section className='main__body'>
       <img src='../public/airbnb-photo.png' className='main__photo' />
-      <h2 className='main__article--title'>Online Experiences</h2>
+      <h1 className='main__article--title'>Online Experiences</h1>
       <p className='main__article--text'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
       </p>
     </section>
