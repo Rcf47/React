62f1d05524b66a9d88a3be21541f67ae2b7e5d97
Vadim Cherepanov <xenos26@gmail.com>
2023-04-01 08:25:22 +0700

    feat: add props for Card

--- scrimba/AirBnbClone/components/Card.jsx
+++ scrimba/AirBnbClone/components/Card.jsx
@@ -1,28 +1,22 @@
 import "./Card.css";
 
-function Card() {
+function Card(props) {
   return (
     <section className="card__body">
       <label className="card__label" >SOLD OUT</label>
-      <img src="../public/katie-zaferes.png" className="card__photo" />
+      <img src={`../public/${props.img}`} className="card__photo" />
       <article className="card__description">
         <div className="card__rating">
           <img src="../public/Star.png" className="card__star" />
-          <span>5.0</span>
-          <span>(6) • </span>
-          <span>USA</span>
+          <span>{props.rating}</span>
+          <span>{props.reviewCount} • </span>
+          <span>{props.country}</span>
         </div>
-        <p className="card__text">Life lessons with Katie Zaferes</p>
-        <p className="card__price">From $136 / <span>person</span></p>
+        <p className="card__text">{props.title}</p>
+        <p className="card__price">From ${props.price} / <span>person</span></p>
       </article>
     </section>
   );
-}
-const person = {
-  name: 'Vadim',
-  lastname: 'Cherepanov',
-  age: 34,
-  diet: 'I don\'t have many'
 }
 
 
--- scrimba/AirBnbClone/src/App.jsx
+++ scrimba/AirBnbClone/src/App.jsx
@@ -1,16 +1,23 @@
-import './App.css'
-import Navbar from '../components/Navbar'
-import Main from '../components/Main'
-import Card from '../components/Card'
+import "./App.css";
+import Navbar from "../components/Navbar";
+import Main from "../components/Main";
+import Card from "../components/Card";
 
 function App() {
   return (
     <>
       <Navbar />
       <Main />
-      <Card />
+      <Card
+        img="katie-zaferes.png"
+        rating="5.0"
+        reviewCount="(6)"
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
