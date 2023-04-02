b7e53a8834a0739866d88c3fedab19a4ac37b966
Vadim Cherepanov <xenos26@gmail.com>
2023-04-03 06:28:21 +0700

    feat: add img meme onClick

--- scrimba/MemeGenerator/components/Meme.jsx
+++ scrimba/MemeGenerator/components/Meme.jsx
@@ -1,17 +1,25 @@
 import "./Meme.css";
+import memesData from "../src/memesData.js"
 
 function Meme() {
+  function getRandomInt() {
+    return Math.floor(Math.random() * (memesData.data.memes.length - 1) + 1)
+  }
+  function addMemeOnClick() {
+    console.log(memesData.data.memes[getRandomInt()].url)
+  }
+
   return (
     <main>
-      <form className="form--main">
+      <div className="form--main">
         <input type="text" placeholder="Shut up" className="form--input" />
         <input
           type="text"
           placeholder="and take my money"
           className="form--input"
         />
-        <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+        <button className="form--button" onClick={addMemeOnClick}>Get a new meme image 🖼</button>
+      </div>
     </main>
   );
 }
