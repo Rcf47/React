506aa2f4ccf7ebbcd5d299cfc61fccbe46df0e9c
Vadim Cherepanov <xenos26@gmail.com>
2023-04-03 04:09:04 +0700

    chore: add style for Header with scrimba

--- scrimba/MemeGenerator/components/Header.jsx
+++ scrimba/MemeGenerator/components/Header.jsx
@@ -4,10 +4,8 @@ import "./Header.css"
 function Header() {
   return (
     <nav className="nav--menu">
-      <div className="nav--logo-wrapper">
-        <img src={trollface} className="nav--logo" />
-        <h1 className="nav--header">MemeGenerator</h1>
-      </div>
+      <img src={trollface} className="nav--logo" />
+      <h1 className="nav--header">MemeGenerator</h1>
       <p className="nav--text">React Course - Project 3</p>
     </nav >
   );
