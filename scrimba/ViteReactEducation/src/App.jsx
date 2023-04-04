00770447234418617a30ddff070be766286dab36
Vadim Cherepanov <xenos26@gmail.com>
2023-04-05 03:36:19 +0700

    feat: add form component

--- scrimba/ViteReactEducation/src/App.jsx
+++ scrimba/ViteReactEducation/src/App.jsx
@@ -1,8 +1,21 @@
 import './App.css'
+import { useState } from "react"
 
 function App() {
+  const [firstName, setFirstName] = useState("")
+
+  function handleChange(event) {
+    console.log(event)
+  }
+
   return (
-    <h1>Hello</h1>
+    <form>
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+      />
+    </form>
   )
 }
 
