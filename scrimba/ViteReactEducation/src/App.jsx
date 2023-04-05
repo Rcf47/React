09a7c745d4b2852d5b1cfb20afdb62e824d6eecd
Vadim Cherepanov <xenos26@gmail.com>
2023-04-05 07:42:10 +0700

    chore: add value for controlled components

--- scrimba/ViteReactEducation/src/App.jsx
+++ scrimba/ViteReactEducation/src/App.jsx
@@ -22,18 +22,21 @@ function App() {
         placeholder="First Name"
         onChange={handleChange}
         name="firstName"
+        value={formData.firstName}
       />
       <input
         type="text"
         placeholder="Last Name"
         onChange={handleChange}
         name="lastName"
+        value={formData.lastName}
       />
       <input
         type="email"
         placeholder="email"
         onChange={handleChange}
         name="email"
+        value={formData.email}
       />
     </form>
   )
