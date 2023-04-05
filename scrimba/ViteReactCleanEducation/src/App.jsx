028901ed8a806a803e85d0ef332205ca062f2a7f
Vadim Cherepanov <xenos26@gmail.com>
2023-04-05 11:31:22 +0700

    feat: complete challenge for form

--- scrimba/ViteReactCleanEducation/src/App.jsx
+++ scrimba/ViteReactCleanEducation/src/App.jsx
@@ -1,9 +1,32 @@
-import './App.css'
-import { useState } from "react"
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    isChecked: true,
+  });
+  console.log(formData);
+  function handleChange(event) {
+    const { name, type, checked, value } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  }
   function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
+    console.log(
+      formData.password === formData.confirmPassword
+        ? "Successfully signed up"
+        : "passwords to not match"
+    );
+    formData.isChecked &&
+      console.log("Thanks for signing up for our newsletter!");
   }
 
   return (
@@ -13,33 +36,40 @@ function App() {
           type="email"
           placeholder="Email address"
           className="form--input"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Password"
           className="form--input"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Confirm password"
           className="form--input"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
         />
 
         <div className="form--marketing">
           <input
             id="okayToEmail"
             type="checkbox"
-
+            name="isChecked"
+            checked={formData.isChecked}
+            onChange={handleChange}
           />
           <label htmlFor="okayToEmail">I want to join the newsletter</label>
         </div>
-        <button
-          className="form--submit"
-        >
-          Sign up
-        </button>
+        <button className="form--submit">Sign up</button>
       </form>
     </div>
-  )
+  );
 }
-export default App
+export default App;
