34bb27420c1b0f5641ae8bdaf8efd750fc7b7a57
Vadim Cherepanov <xenos26@gmail.com>
2023-04-09 13:01:36 +0700

    feat: add commit with diff put

--- scrimba/ViteReactTenziesGame/src/App.jsx
+++ scrimba/ViteReactTenziesGame/src/App.jsx
@@ -8,8 +8,13 @@ function App() {
   const [winCondition, setWinCondition] = useState(false);
 
   useEffect(() => {
-    console.log('Dice state effect changes')
-  }, [arrayDie])
+    const allHeld = arrayDie.every((die) => die.isHeld);
+    const allValue = arrayDie.every((die) => die.value === arrayDie[0].value);
+    if (allHeld && allValue) {
+      setWinCondition(true);
+      console.log("you won");
+    }
+  }, [arrayDie]);
 
   function randomInt() {
     return Math.ceil(Math.random() * 6);
@@ -20,30 +25,43 @@ function App() {
       id: nanoid(),
       value: randomInt(),
       isHeld: false,
-    }
+    };
   }
   function generateDiceArray(numberItem) {
     let objInt = [];
     for (let i = 0; i < numberItem; i++) {
-      objInt.push(getNewDice())
+      objInt.push(getNewDice());
     }
     return objInt;
   }
 
   function handleClick() {
-    setArrayDie(previousDiceArray => previousDiceArray.map(item => item.isHeld === true ? item : getNewDice()));
+    setArrayDie((previousDiceArray) =>
+      previousDiceArray.map((item) =>
+        item.isHeld === true ? item : getNewDice()
+      )
+    );
   }
   function holdDice(id) {
-    setArrayDie(previousDiceArray => previousDiceArray.map(item => item.id === id ? { ...item, isHeld: !item.isHeld } : item))
+    setArrayDie((previousDiceArray) =>
+      previousDiceArray.map((item) =>
+        item.id === id ? { ...item, isHeld: !item.isHeld } : item
+      )
+    );
   }
 
   let dieArray = arrayDie.map((item) => (
-    <Die key={item.id} value={item.value} isHeld={item.isHeld} holdDice={() => holdDice(item.id)} />
+    <Die
+      key={item.id}
+      value={item.value}
+      isHeld={item.isHeld}
+      holdDice={() => holdDice(item.id)}
+    />
   ));
 
   return (
     <main className="board">
-    <h1 className="title">Tenzies</h1>
+      <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
