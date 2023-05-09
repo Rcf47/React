bae68c952964cd5315476c3bb9b606ea3b324a34
Vadim Cherepanov <xenos26@gmail.com>
2023-05-09 23:02:20 +0700

    feat: add start about page

---
+++ scrimba/React_router/firstScrimbaProject/Van_Project/src/About.jsx
@@ -0,0 +1,7 @@
+function About() {
+  return (
+    <h1>This is all about</h1>
+  )
+}
+
+export default About
--- scrimba/React_router/firstScrimbaProject/Van_Project/src/App.jsx
+++ scrimba/React_router/firstScrimbaProject/Van_Project/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
+import About from './About'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />:w
       </Routes>
     </BrowserRouter>
   )
