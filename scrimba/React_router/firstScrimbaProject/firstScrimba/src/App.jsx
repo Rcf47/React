0b2e180553c40d6dc6f48e97ac5133e40459ffc3
Vadim Cherepanov <xenos26@gmail.com>
2023-05-09 22:25:45 +0700

    feat: add Link in react router

--- scrimba/React_router/firstScrimbaProject/firstScrimba/src/App.jsx
+++ scrimba/React_router/firstScrimbaProject/firstScrimba/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import About from './About'
 import Home from './Home'
 
 function App() {
   return (
     <BrowserRouter>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
